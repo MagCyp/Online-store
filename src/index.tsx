fb2b480fba49f13fc52e7aa89a714c7d55fab62f
yura12481 <yura124812481@gmail.com>
2023-11-06 12:17:41 +0200

    Packages react-redux and redux-toolkit were installed, folder hooks was created, userSlice, was successfuly created

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
