28b83b6885677fa62fda81cf990579246a6eb644
Invs_Cypher <dimares53@gmail.com>
2023-11-21 13:54:41 +0100

    import sort

--- src/store/slices/user/userReducers.ts
+++ src/store/slices/user/userReducers.ts
@@ -1,5 +1,6 @@
-import { UserState } from './types';
 import { PayloadAction } from '@reduxjs/toolkit';
+
+import { UserState } from './types';
 
 export const setUser = (state: UserState, action: PayloadAction<UserState>) => {
   state.email = action.payload.email;
