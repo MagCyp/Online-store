e311ae7cf19070e364827243b31d04e510574811
Invs_Cypher <dimares53@gmail.com>
2023-12-22 17:49:16 +0100

    Added breadcrumb & replace all a to Link

---
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import ArrowRight from '@components/icons/ArrowRight';
+import Button from '@components/button/Button';
+
+import { Props } from '@components/breadcrumb/types';
+
+import styles from '@components/breadcrumb/Breadcrumb.module.scss';
+
+const list: Record<string, string> = {
+  mice: 'Mouse',
+  mouseMats: 'Mouse mats',
+  keyboards: 'Keyboard',
+  joysticks: 'Joystick and controller',
+  headsets: 'Headsets',
+  gamingChairs: 'Gaming chairs',
+};
+
+const BreadCrumb: FC<Props> = ({ category, name }) => {
+  const convertCategory = (category: string): string => {
+    if (category in list) {
+      return list[category];
+    } else {
+      return category;
+    }
+  };
+
+  const newCat = convertCategory(category);
+
+  return (
+    <div className={styles['container']}>
+      {!name && <h2 className="regular">{newCat}</h2>}
+      <div className={styles['breadcrumbs']}>
+        <Button
+          className="link-gray small"
+          type="button"
+          text="Home"
+          href="/"
+        />
+        <ArrowRight size="small" />
+        <Link to={`/catalog/${category}`}>{newCat}</Link>
+        {name && (
+          <>
+            <ArrowRight size="small" />
+            <span>{name}</span>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BreadCrumb;
--- src/components/header/dropDownItem/DropDownItem.tsx
+++ src/components/header/dropDownItem/DropDownItem.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import ArrowRightUp from '@components/icons/ArrowRightUp';
 import Badge from '@components/badge/Badge';
@@ -10,22 +11,21 @@ import styles from '@components/header/dropDownItem/DropDownItem.module.scss';
 
 const DropDownItem: FC<Props> = ({ icon, text, href, isNew, listItem }) => {
   return (
-    <div
+    <Link
+      to={href}
       className={`${styles['drop-down-item']} ${
         listItem ? styles['list-item'] : ''
       }`}
     >
       <div className={styles['text-badge']}>
         {icon ? icon : <Search size="small" />}
-        <a href={href} className={styles['text']}>
-          {text}
-        </a>
+        <p className={styles['text']}>{text}</p>
         {isNew && <Badge className="success s" text="New" />}
       </div>
       <div className={styles['icon-hovered']}>
         <ArrowRightUp size="medium" />
       </div>
-    </div>
+    </Link>
   );
 };
 
