12c6b4f302288dbad966b3bd4d301d0bc0e2beaa
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-05-20 22:11:27 +0200

    style: Add margin to checkout stages

--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from '@/components/checkout/Checkout.module.scss'
+import Container from '@components/container/Container';
 
 const Checkout: React.FC = () => {
-    return (
+    return ( 
+    <Container>
         <div>
-            <div>
+            <div className={styles['checkoutStages']}>
                 <h2>My orders</h2>
                 <p>Home &gt; My orders</p>
             </div>
         </div>
+        </Container>
     )
 }
 
